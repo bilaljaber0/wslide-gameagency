ee2e62129cddc9b9c704427c8fd033d970f304c4
mastersnake458 <lovetobeme458@gmail.com>
2025-10-09 05:44:55 +0300

    Fixed folder name and CSS

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const cards = document.querySelectorAll('.card');
-  cards.forEach((card, i) => {
-    setTimeout(() => card.classList.add('visible'), i * 150);
-  });
-});
